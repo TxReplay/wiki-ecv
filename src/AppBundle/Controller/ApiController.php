270d860f2241a7cd707423953f0a02ccb156441a
Valentin Commenge <txreplay@gmail.com>
2017-05-11 17:30:23 +0200

    Api controller and orm behaviors

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -5,17 +5,18 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
- * @Route("/{_locale}")
+ * @Route("/api/%api_version%")
  */
-class FrontController extends Controller
+class ApiController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/user/", name="api_user")
      */
     public function indexAction(Request $request)
     {
-        return $this->render('front/index.html.twig', []);
+        return new JsonResponse();
     }
 }
