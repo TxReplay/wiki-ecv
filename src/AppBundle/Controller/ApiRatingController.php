92b058846cde06ef375cad559d455f552e539934
Valentin Commenge <txreplay@gmail.com>
2017-05-12 11:51:40 +0200

    get one user api workin

--- src/AppBundle/Controller/ApiRatingController.php
+++ src/AppBundle/Controller/ApiRatingController.php
@@ -2,9 +2,16 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Controller\ApiController;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\ViewHandler;
+use FOS\RestBundle\View\View;
 
-class ApiRatingController extends ApiController
+class ApiRatingController extends FOSRestController
 {
     public function postRatingAction() {}
 
--- src/AppBundle/Controller/ApiUserController.php
+++ src/AppBundle/Controller/ApiUserController.php
@@ -2,16 +2,36 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Controller\ApiController;
-use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\ViewHandler;
+use FOS\RestBundle\View\View;
 
-class ApiUserController extends ApiController
+
+class ApiUserController extends FOSRestController
 {
     public function postUserAction() {}
 
-    public function getUserAction($id) {
-        return new JsonResponse(['id' => $id]);
+    /**
+     * @Rest\View()
+     * @Rest\Get("/user/{user_id}")
+     */
+    public function getUserAction(Request $request)
+    {
+        $user = $this->getUserRepository()->find($request->get('user_id'));
+
+        if (empty($user)) {
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return [
+            'id' => $user->getId(),
+            'username' => $user->getUsername(),
+            'email' => $user->getEmail()
+        ];
     }
 
     public function putUserAction($id) {}
@@ -22,4 +42,9 @@ class ApiUserController extends ApiController
 
     public function getUserLogoutAction() {}
 
+    private function getUserRepository()
+    {
+        return $this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:User');
+
+    }
 }
