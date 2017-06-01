e78b0b18b943fc5a8b743213da283b94bf3e88bd
Nicolas Heine <nicolas.heine11@gmail.com>
2017-06-01 14:03:21 +0200

    Afichage des 3 derniers article sur la home

--- web/js/controller/frontcontroller.js
+++ web/js/controller/frontcontroller.js
@@ -1,7 +1,19 @@
 app.controller('FrontController',
     ['$scope', '$http', 'user',
         function($scope, $http, user){
+            var ctrl = this;
             $scope.user = user;
+
+            $http.get('/api/v1/page/last?limit=3&offset=1').then(
+                function(success){
+                    ctrl.pages = success.data;
+                    console.log(ctrl.pages);
+                },
+                function(error){
+                    console.log(error);
+                }
+            )
+
         }
     ]
 );
